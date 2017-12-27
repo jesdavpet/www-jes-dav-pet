9b1abeb79f3b26485ed7b2b3654493ba1cf8d26e
Jesse Peterson <jesdavpet@gmail.com>
2017-12-27 16:56:21 -0500

    Merge branch 'gatsby-scaffolding'

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: { title: `Jesse David Peterson` },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src`
+      }
+    }
+  ]
+}
